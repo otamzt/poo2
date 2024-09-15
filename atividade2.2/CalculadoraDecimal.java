5e729c43f644d6f8622b9a4281d8bd4b556b5fd9
otamzt <128203343+otamzt@users.noreply.github.com>
2024-09-15 14:59:42 -0300

    Create CalculadoraDecimal.java

---
+++ atividade2.2/CalculadoraDecimal.java
@@ -0,0 +1,13 @@
+public class CalculadoraDecimal {
+    public int somar(int a, int b) {
+        return a + b;
+    }
+
+    public int subtrair(int a, int b) {
+        return a - b;
+    }
+
+    public int multiplicar(int a, int b) {
+        return a * b;
+    }
+}

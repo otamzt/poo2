55d661268ce21b8e41af99c744f6c60a42165b81
otamzt <128203343+otamzt@users.noreply.github.com>
2024-10-13 18:07:43 -0300

    Create CintoSeguranca.java

---
+++ atividade5.2/CintoSeguranca.java
@@ -0,0 +1,9 @@
+public class CintoSeguranca {
+    public void travar() {
+        System.out.println("Cinto de segurança travado.");
+    }
+
+    public void destravar() {
+        System.out.println("Cinto de segurança destravado.");
+    }
+}

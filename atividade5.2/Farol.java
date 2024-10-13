9de48533aa57cc9edf4a0102d85bc2402c265d04
otamzt <128203343+otamzt@users.noreply.github.com>
2024-10-13 18:08:43 -0300

    Create Farol.java

---
+++ atividade5.2/Farol.java
@@ -0,0 +1,9 @@
+public class Farol {
+    public void acender() {
+        System.out.println("Faróis acesos.");
+    }
+
+    public void apagar() {
+        System.out.println("Faróis apagados.");
+    }
+}

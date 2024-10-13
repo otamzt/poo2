811a38ee6b970e43e1a5351bf87dbf90f46354ee
otamzt <128203343+otamzt@users.noreply.github.com>
2024-10-13 18:09:40 -0300

    Create Main.java

---
+++ atividade5.2/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        CarroFacade carro = new CarroFacade();
+
+        // Iniciar a viagem
+        carro.iniciarViagem("Estação FM 99.5");
+
+        // Finalizar a viagem
+        carro.finalizarViagem();
+    }
+}
